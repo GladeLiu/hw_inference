d8fa34078c7050544943e0296e6a77beb98fd300
Chenang Liu <liuchenang@gmail.com>
2015-03-01 23:49:59 -0500

    Add R script
    Add R script

---
+++ server.R
@@ -0,0 +1,33 @@
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input,output){
+  
+  # Expression that generates a histogram. The expression is
+  # wrapped in a call to renderPlot to indicate that:
+  #
+  #  1) It is "reactive" and therefore should re-execute automatically
+  #     when inputs change
+  #  2) Its output type is a plot
+  
+  output$distPlot<-renderPlot({
+    library(ggplot2);
+    N<-input$samples; prob<-input$prob;
+    sd<-sqrt((1/((1-prob)^4))*prob*(1-prob));
+    gp<-prob/(1-prob);
+    gmean<-rep(0,5000);
+    set.seed(2015);
+    for (i in 1:5000){
+      sample<-rbinom(N,1,prob);
+      gmean[i]<-(mean(sample))/(1-mean(sample));
+    }
+    hist<-sqrt(N)*(gmean-gp);
+    data<-data.frame(hist=hist);
+    p<-ggplot(data,aes(hist))
+    p+geom_histogram(position="identity",
+                     alpha=0.7,
+                     aes(y=..density..,fill=..density..))+
+      stat_density(geom="line",aes(colour="Density Estimation"))+
+      stat_function(fun=dnorm,args=list(mean=0,sd=sd),aes(hist,colour="Normal Distribution"));
+  })
+})
---
+++ ui.R
@@ -0,0 +1,30 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Delta Method"),
+  
+  # Sidebar with a slider input for the number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("samples",
+                  "Number of Samples:",
+                  min=200,
+                  max=1500,
+                  value=1000),
+      
+      sliderInput("Prob",
+                  "probability:",
+                  min=0.1,
+                  max=0.9,
+                  value=0.5)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
